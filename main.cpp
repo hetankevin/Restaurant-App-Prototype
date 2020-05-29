2792195689149ef29acc385cd4d77449bc19220c
danlliu <danlliu@umich.edu>
2020-05-29 11:13:17 -0400

    Added storage for food places

--- main.cpp
+++ main.cpp
@@ -20,6 +20,34 @@
 
 using namespace std;
 
+struct FoodPlace {
+    string name;
+    int rating; // 42 = 4.2 rating / 5
+    int price; // 1 = $, 2 = $$, ..., 4 = $$$$
+    bool dine_in;
+    bool take_out;
+};
+
+class FoodLocations {
+    
+    vector<FoodPlace> places;
+    unordered_map<string, int> namelookup;
+    
+public:
+    
+    FoodLocations() {}
+    
+    void addPlace(const FoodPlace &fp) {
+        places.push_back(fp);
+        namelookup[fp.name] = (int) places.size() - 1;
+    }
+    
+    int searchPlace(const string &name) {
+        return (namelookup.find(name) != namelookup.end()) ? namelookup[name] : -1;
+    }
+    
+};
+
 int main(int argc, char *argv[]) {
     cout << "Hello world" << endl;
     return 0;
