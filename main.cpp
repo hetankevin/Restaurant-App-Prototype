70672b2eea4af088383836fbc71c1d75bf320bab
danlliu <danlliu@umich.edu>
2020-05-29 09:48:22 -0400

    Added #include declarations for common libraries

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cassert>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
