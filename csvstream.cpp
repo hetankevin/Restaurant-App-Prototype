5eeac4332fafee4e3e8af2e7d6b214b8dcdea79a
danlliu <danlliu@umich.edu>
2020-05-29 12:12:50 -0400

    Added operator[] to FoodLocations, started csvstream.h, .cpp

---
+++ csvstream.cpp
@@ -0,0 +1,11 @@
+//
+//  csvstream.cpp
+//  stunning-potato
+//
+//  Created by Daniel Liu on 20-5-29.
+//  Copyright © 2020 Daniel Liu. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+
---
+++ csvstream.h
@@ -0,0 +1,13 @@
+//
+//  csvstream.h
+//  stunning-potato
+//
+//  Created by Daniel Liu on 20-5-29.
+//  Copyright © 2020 Daniel Liu. All rights reserved.
+//
+
+#ifndef csvstream_h
+#define csvstream_h
+
+
+#endif /* csvstream_h */
