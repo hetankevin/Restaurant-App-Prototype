4dccf23fdd5911d5b4231d5bce6ea447f9ffb9e4
danlliu <danlliu@umich.edu>
2020-06-05 14:02:13 -0400

    js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
+    console.log(req.url);
     switch (req.url) {
-        case "css/styles.css":
+        case "/css/styles.css":
             fs.readFile('html/css/styles.css', function(err, data) {
                 res.writeHead(200, {'Content-Type': 'text/css'});
                 res.write(data);
                 return res.end();
             });
+            break;
         default:
             fs.readFile('html/index.html', function(err, data) {
                 res.writeHead(200, {'Content-Type': 'text/html'});
