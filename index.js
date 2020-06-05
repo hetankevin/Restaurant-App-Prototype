d162174287f78058e710c821574e1283f15254e1
danlliu <danlliu@umich.edu>
2020-06-05 14:01:50 -0400

    js

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    switch (req.url) {
+        case "css/styles.css":
+            fs.readFile('html/css/styles.css', function(err, data) {
+                res.writeHead(200, {'Content-Type': 'text/css'});
+                res.write(data);
+                return res.end();
+            });
+        default:
+            fs.readFile('html/index.html', function(err, data) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            });
+    }
+
+}).listen(8080);
