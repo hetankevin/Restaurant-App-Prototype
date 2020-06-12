49d2e2f53d4a1c5aeaa8114b13e5433c2bad5eda
danlliu <danlliu@umich.edu>
2020-06-12 11:04:45 -0400

    Index bla bla bla

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.get("/", (request, response) => {
-    response.sendFile(path.join(__dirname, './html/index.html'));
+    //response.sendFile(path.join(__dirname, './html/index.html'));
+    response.render('html/restaurants', {pageTitle: 'Welcome'});
 });
 
 app.listen(port, () => {
