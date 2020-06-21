ba3ea61b3dd604eec79af64c3100876fd9b53e67
danlliu <danlliu@umich.edu>
2020-06-21 10:48:40 -0400

    Updates

--- StunningPotato/routes/index.js
+++ StunningPotato/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Stunning Potato' });
+  fs.readFile('/annarbor.json', (err, data) => {
+    console.log(data);
+    console.log(err);
+    let json_data = data.toJSON();
+    res.render('index', { title: 'Stunning Potato', places: json_data });
+  });
 });
 
 module.exports = router;
