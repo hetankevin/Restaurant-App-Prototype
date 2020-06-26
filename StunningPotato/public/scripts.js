4e409c83c0c8a10a88b88ad6c2547cb6b6165c16
Kevin Hao En Tan <kevtan@umich.edu>
2020-06-27 00:44:51 +0800

    room logic

--- StunningPotato/public/scripts.js
+++ StunningPotato/public/scripts.js
@@ -1,9 +1,14 @@
 
-var socket = io.connect('http://localhost');
+/*
+var socket = io.connect('http://localhost:3000');
+
 socket.on('connect', function() {
     socket.emit('addUser', prompt("What's your name?"));
-});
+})
+;
+*/
+/*
 socket.on('updateChat', function (username, data) {
     $('#conversation').append('<b>'+username + ':</b> ' + data + '<br>');
 });
-
+*/
