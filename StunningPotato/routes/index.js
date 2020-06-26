85431c7e214656b5e794975a35fa9fa225cb2340
Kevin Hao En Tan <kevtan@umich.edu>
2020-06-26 22:19:33 +0800

    init server.io

--- StunningPotato/routes/index.js
+++ StunningPotato/routes/index.js
@@ -1,14 +1,17 @@
+
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   fs.readFile('public/annarbor.json', (err, data) => {
     if (err) throw err;
     let json_data = JSON.parse(data);
     res.render('index', { title: 'Stunning Potato', places: json_data });
   });
+
 });
 
 module.exports = router;
--- StunningPotato/routes/users.js
+++ StunningPotato/routes/users.js
@@ -1,22 +1,16 @@
 const io = require('socket.io-client');
 var express = require('express');
 var router = express.Router();
-var url = require('url');
+
 var socket = io.connect("http://localhost:3000/");
 
-function getFormattedUrl(req) {
-  return url.format({
-    protocol: req.protocol,
-    host: req.get('host')
-  });
-}
+socket.on('user-number', function(userIndex){
+  console.log("User", userIndex, "has connected");
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-  socket.on('user-number', function(userIndex){
-    res.render('users', {user: userIndex});
-  })
+  res.send('index', {user: userIndex});
 });
 
 
