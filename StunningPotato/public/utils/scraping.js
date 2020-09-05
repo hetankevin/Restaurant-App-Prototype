50900d990b918fe391a5781b4fb97c0129ada781
Kevin Hao En Tan <kevtan@umich.edu>
2020-09-05 14:33:35 -0500

    beepboop scraper done

--- StunningPotato/public/utils/scraping.js
+++ StunningPotato/public/utils/scraping.js
@@ -6,10 +6,54 @@
 //     east: number
 // }
 
-function scrapeRegion(region) {
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjJLkTKGdauHGUkzoeEoyS0ZoD4wUnlx8&libraries=places">
+let map;
+let service;
+let infowindow;
+
+
+function scrapeRegion(region, typeIn) {
     // write your code here
-    return [{}];
+    let places = new Set();
+    let resultSize = 0;
+    let request = new google.maps.places.PlaceSearchRequest(bounds=region, type=typeIn);
+    let service = new google.maps.places.PlacesService(document.createElement('div'));
+    service.nearbySearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            resultSize = results.length;
+            for (let i = 0; i < results.length; i++) {
+                //put into places, removing duplicates
+                places.add(results[i]);
+            }
+        }
+    });
+    if (resultSize >= 60){
+        let north = region["north"];
+        let south = region["south"];
+        let east = region["east"];
+        let west = region["west"]
+        let midVert = (region["north"] + region["south"])/2;
+        let midHor = (region["east"] + region["west"])/2;
+
+        //top left
+        scrapeRegion({"north" : north, "south" : midVert,
+            "east" : midHor, "west" : west }, typeIn).forEach(element => places.add(element));
+        //top right
+        scrapeRegion({"north": north, "south" : midVert,
+            "east" : east, "west" : midHor}, typeIn).forEach(element => places.add(element));
+        //bottom left
+        scrapeRegion({"north": midVert, "south" : south,
+            "east" : midHor, "west" : west}, typeIn).forEach(element => places.add(element));
+        //bottom right
+        scrapeRegion( {"north": midVert, "south": south,
+            "east" : east, "west" : midHor}, typeIn).forEach(element => places.add(element));
+    }
+    return places;
 }
+
+
 
 // for node
 const MongoClient = require('mongodb').MongoClient;
