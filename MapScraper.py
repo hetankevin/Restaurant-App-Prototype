f251884e9c0971a73c9f96acb19967be4d26ffae
hetankevin <55823167+hetankevin@users.noreply.github.com>
2020-09-26 17:47:50 -0500

    Update MapScraper.py

--- MapScraper.py
+++ MapScraper.py
@@ -1,52 +1,75 @@
-import pandas as pd, numpy as np
-import requests
-import json
-import time
+// region has format of
+// {
+//     north: number
+//     south: number
+//     west: number
+//     east: number
+// }
 
-final_data = []
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+let map;
+let service;
+let infowindow;
 
-# Parameters
-coordinates = ['42.2808,-83.7430']
-keywords = ['restaurant']
-radius = '1000'
-api_key = 'AIzaSyCjJLkTKGdauHGUkzoeEoyS0ZoD4wUnlx8' #insert your Places API
 
-for coordinate in coordinates:
-    for keyword in keywords:
-        url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+coordinate+'&radius='+str(radius)+'&keyword='+str(keyword)+'&key='+str(api_key)
+function scrapeRegion(region, typeIn) {
+    // write your code here
+    let places = new Set();
+    let resultSize = 0;
+    let request = new google.maps.places.PlaceSearchRequest(bounds=region, type=typeIn);
+    let service = new google.maps.places.PlacesService(document.createElement('div'));
+    service.nearbySearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            resultSize = results.length;
+            for (let i = 0; i < results.length; i++) {
+                //put into places, removing duplicates
+                places.add(results[i]);
+            }
+        }
+    });
+    if (resultSize >= 60){
+        let north = region["north"];
+        let south = region["south"];
+        let east = region["east"];
+        let west = region["west"]
+        let midVert = (region["north"] + region["south"])/2;
+        let midHor = (region["east"] + region["west"])/2;
 
-while True:
-    print(url)
-    respon = requests.get(url)
-    jj = json.loads(respon.text)
-    results = jj['results']
+        //top left
+        scrapeRegion({"north" : north, "south" : midVert,
+            "east" : midHor, "west" : west }, typeIn).forEach(element => places.add(element));
+        //top right
+        scrapeRegion({"north": north, "south" : midVert,
+            "east" : east, "west" : midHor}, typeIn).forEach(element => places.add(element));
+        //bottom left
+        scrapeRegion({"north": midVert, "south" : south,
+            "east" : midHor, "west" : west}, typeIn).forEach(element => places.add(element));
+        //bottom right
+        scrapeRegion( {"north": midVert, "south": south,
+            "east" : east, "west" : midHor}, typeIn).forEach(element => places.add(element));
+    }
+    return places;
+}
 
-    for result in results:
-        name = result.get('name')
-        place_id = result .get('place_id')
-        lat = result['geometry']['location']['lat']
-        lng = result['geometry']['location']['lng']
-        rating = result.get('rating')
-        reviews = result.get('user_ratings_total')
-        types = result.get('types')
-        vicinity = result.get('vicinity')
-        price_level = result.get('price_level')
-        photos = result.get('photos')
-        status = result.get('business_status')
-        temp_closed = result.get('permanently_closed')
-        data = [name, place_id, lat, lng, rating, reviews, types, vicinity, price_level, photos, status, temp_closed]
-        final_data.append(data)
 
-    time.sleep(5)
-    if 'next_page_token' not in jj:
-        break
-    else:
-        next_page_token = jj['next_page_token']
-        url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='+str(api_key)+'&pagetoken='+str(next_page_token)
 
-labels = ['Place Name','Place ID', 'Latitude', 'Longitude', 'Ratings', 'Reviews', 'Types', 'Vicinity', 'Price Level', 'Photos', 'Status', 'Temp Closed']
+// for node
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://user_1:user_1@restaurants.t4ma9.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
-export_dataframe_1_medium = pd.DataFrame.from_records(final_data, columns=labels)
+// data is array of objects
 
-export_dataframe_1_medium.to_json('annarbor.json', orient='records')
+//update this to get photos for each element
+function uploadToDatabase(data) {
+    const collection = client.db("Restaurants").collection("Restaurants");
+    data.forEach(element => collection.update(
+        filter= {"Place ID" : element["Place ID"]}, update=element, upsert=true));
+}
 
